7d07f89a02950542eff432f76e38e4f8f713eabe
turican0 <41465132+turican0@users.noreply.github.com>
2022-09-14 21:29:52 +0200

    update 220914-02

--- sub_main.h
+++ sub_main.h
@@ -3,8 +3,8 @@
 #define _WORD  uint16
 #define _DWORD uint32
 
-extern __int16 word_A9990;
-extern int dword_A99A0;
-extern int dword_AE450;
+//extern __int16 word_A9990;
+extern int dword_A99B8;
+extern int dword_AE440;
 
 int sub_main(int argc, const char** argv, const char** envp);
